8c2c3804df95457b3b24872c977f112959a7b0b1
David Orbang <orbangdavid@yahoo.com>
2024-03-04 09:12:18 +0000

    Learing Java day2

---
+++ Learning Java basics/src/Part2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Part2 {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArray = new ArrayList<>();
+
+        stringArray.add("Hello");
+        stringArray.add("World");
+        stringArray.add("!");
+
+        System.out.println(stringArray);
+        System.out.println(stringArray.get(1));
+
+    }
+
+}
