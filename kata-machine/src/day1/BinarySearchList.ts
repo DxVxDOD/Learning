25a2f4383d22230584f3b95866383a8b44479ec9
David Orbang <orbangdavid@yahoo.com>
2024-03-11 18:48:52 +0000

    binary search

---
+++ kata-machine/src/day1/BinarySearchList.ts
@@ -0,0 +1,19 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const midPoint = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[midPoint];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            hi = midPoint;
+        } else {
+            lo = midPoint + 1;
+        }
+    } while (lo < hi);
+
+    return false;
+}
