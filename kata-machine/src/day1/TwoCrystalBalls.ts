a00ab03e27db49d8395863d9eded598755eaff4b
David Orbang <orbangdavid@yahoo.com>
2024-03-11 19:54:41 +0000

    TwoCrystalBalls

--- kata-machine/src/day1/TwoCrystalBalls.ts
+++ kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmoumt = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmoumt;
+    for (; i < breaks.length; i += jumpAmoumt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpAmoumt;
+    for (let j = 0; j < jumpAmoumt && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
 
+    return -1;
 }
