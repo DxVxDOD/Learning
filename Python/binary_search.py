88f5ad79eff2f2c5d38581dbf7a5a9d209689687
David Orbang <orbangdavid@yahoo.com>
2024-03-21 19:02:22 +0000

    recursive binary search

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,3 +1,6 @@
+import random
+
+
 def binary_search(arr: list, item: int) -> int:
     low = 0
     high = len(arr) - 1
@@ -13,3 +16,20 @@ def binary_search(arr: list, item: int) -> int:
 
 
 print(binary_search([1, 3, 5, 7, 9], 9))
+
+
+def recursive_binary_search(nums: list, item: int) -> int:
+    mid = len(nums) // 2
+    if mid == 1 & nums[mid] != item:
+        return -1
+    if nums[mid] == item:
+        return nums[mid]
+    if nums[mid] > item:
+        return recursive_binary_search(nums[0:mid], item)
+    return recursive_binary_search(nums[mid:], item)
+
+
+random_ints = [3, 7, 10, 12, 14, 18, 25, 33, 45, 50, 53, 62, 67, 68, 79, 84, 88, 89, 91, 96]
+
+
+print(recursive_binary_search(random_ints, 68))
