6064367997cf9fdc13225feadc39f38e5dd14e3a
DxVxDOD <orbangdavid@yahoo.com>
2024-09-01 12:00:14 +0300

    learning go

---
+++ go/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "text"
+	fmt.Println(a)
+
+	var b int = 10
+	fmt.Println(b)
+
+	var c, d int = 10, 12
+	fmt.Println(c, d)
+
+	e := "no var text"
+	f := 10
+	println(e, f)
+}
