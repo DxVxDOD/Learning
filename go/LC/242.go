24fe0df6d5eeca85d7a27cd8832382260c9970c3
DxVxDOD <orbangdavid24@gmail.com>
2026-02-03 18:37:43 +0000

    make file

--- go/LC/242.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	s := "car"
-	countMap := make(map[byte]int)
-	sByte := []byte(s)
-}
---
+++ go/LC/3110.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "anagram"
+	fmt.Println(str[0])
+}
