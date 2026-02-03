ea74ca493734b9a352dc1aa978f8a39e159897f8
DxVxDOD <orbangdavid24@gmail.com>
2026-02-03 21:52:17 +0000

    constants

--- go/learning-go/ch1/hello.go
+++ go/learning-go/ch1/hello.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello Ppppuuuffy!  ")
+	x := 200
+	var b byte = 255
+
+	fmt.Println("x: ", x, "b: ", b)
+
+	sum1 := x + int(b)
+	sum2 := byte(x) + b
+
+	fmt.Println("byte(x): ", byte(x), "int(b): ", int(b))
+
+	fmt.Println("sum1: ", sum1, "sum2: ", sum2)
 }
