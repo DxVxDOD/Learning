18c6d64e6a55f8b73a4690e67c37a9d0c4e1b517
DxVxDOD <orbangdavid24@gmail.com>
2026-02-25 06:46:28 +0000

    yesterday

--- go/learning-go/ch4/main.go
+++ go/learning-go/ch4/main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
+	varInIf()
+	skipOuter()
+}
+
+func skipOuter() {
+	strs := []string{"LOOOOOOOOOOL", "BIIIIG"}
+outer:
+	for _, v := range strs {
+		for i, strV := range v {
+			fmt.Println(i, strV, string(strV))
+			if strV == 'O' {
+				continue outer
+			}
+		}
+	}
+}
+
+func varInIf() {
+	if n := rand.Intn(10); n == 0 {
+		fmt.Println("LOOOOOOOOOOL")
+	} else if n < 5 {
+		fmt.Println("SMAAAL")
+	} else {
+		fmt.Println("BIIIIG")
+	}
 }
