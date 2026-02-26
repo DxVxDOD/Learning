4f2e6715c53eb2aada515ca3678a33d27a0643cb
DxVxDOD <orbangdavid24@gmail.com>
2026-02-26 09:03:06 +0000

    LG

--- go/learning-go/ch5/main.go
+++ go/learning-go/ch5/main.go
@@ -2,14 +2,78 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
 	"strconv"
 )
+
+type Person struct {
+	fName string
+	lName string
+	age   int
+}
 
 func main() {
 	fmt.Println(namedReturn())
 	fmt.Println(blancReturn())
 	fmt.Println(nonesense())
+	fmt.Println(calculator())
+	fmt.Println(anonymus())
+	clousreInSort()
+	cat()
+}
 
+func cat() {
+	if len(os.Args) < 2 {
+		log.Fatal("No file specified")
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	data := make([]byte, 2048)
+	for {
+		count, err := f.Read(data)
+		os.Stdout.Write(data[:count])
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			break
+		}
+	}
+}
+
+func clousreInSort() {
+	people := []Person{
+		{"Puffy", "Puff", 27},
+		{"Puf", "Pu", 26},
+		{"Puffu", "Puffulet", 25},
+	}
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].fName < people[j].fName
+	})
+	fmt.Println("Sorted by first name: ", people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].age < people[j].age
+	})
+	fmt.Println("Sorted by last age: ", people)
+}
+
+func anonymus() int {
+	f := func(a int) int {
+		return a * a
+	}
+	return f(4)
+}
+
+func calculator() int {
+	var result int
 	expressions := [][]string{
 		{"2", "+", "3"},
 		{"2", "-", "3"},
@@ -40,9 +104,9 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-		result := opFunc(p1, p2)
-		fmt.Println(result)
+		result = opFunc(p1, p2)
 	}
+	return result
 }
 
 func add(i, j int) int { return i + j }
