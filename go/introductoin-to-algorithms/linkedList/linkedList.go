76ad03bf4c5fd7c9712325616d665ab4b1ebd83b
David Orban <orbangdavid24@gmail.com>
2026-03-07 15:36:35 +0000

    LI

---
+++ go/introductoin-to-algorithms/linkedList/linkedList.go
@@ -0,0 +1,4 @@
+// Package linkedlist respresentes a data structure called linked lists. This data structure is sequential but not contiguous in memory.
+package linkedlist
+
+type Linkedlist struct{}
