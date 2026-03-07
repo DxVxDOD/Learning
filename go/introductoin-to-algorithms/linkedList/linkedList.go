ce52d1d54913999fa459f8ba941832fa5481753e
DxVxDOD <orbangdavid24@gmail.com>
2026-03-07 19:21:54 +0000

    buil ll nad get all

--- go/introductoin-to-algorithms/linkedList/linkedList.go
+++ go/introductoin-to-algorithms/linkedList/linkedList.go
@@ -1,4 +1,78 @@
 // Package linkedlist respresentes a data structure called linked lists. This data structure is sequential but not contiguous in memory.
 package linkedlist
 
-type Linkedlist struct{}
+import (
+	"errors"
+)
+
+type Linkedlist[T any] struct {
+	Item   T
+	next   *Linkedlist[T]
+	Head   *Linkedlist[T]
+	length int
+}
+
+func (l *Linkedlist[T]) Build(arr []T) *Linkedlist[T] {
+	var nextNode *Linkedlist[T]
+	l.Item = arr[0]
+	l.length = 1
+	nextNode = l
+	nextNode.Head = l
+	arrLen := len(arr)
+	for i := 1; i < arrLen; i++ {
+		nextNode = nextNode.Add(arr[i])
+	}
+	return nextNode
+}
+
+func (l *Linkedlist[T]) GetAll() []T {
+	all := make([]T, l.length)
+	nextNode := l.Head
+
+	i := 0
+	for nextNode != nil {
+		all[i] = nextNode.Item
+		i++
+		nextNode = nextNode.next
+	}
+
+	return all
+}
+
+func (l *Linkedlist[T]) Len() int {
+	return l.length
+}
+
+func (l *Linkedlist[T]) init(val T) *Linkedlist[T] {
+	newNode := &Linkedlist[T]{Item: val}
+	l = newNode
+	l.Head = newNode
+	l.length = 1
+	return newNode
+}
+
+func (l *Linkedlist[T]) Add(val T) *Linkedlist[T] {
+	if l == nil {
+		return l.init(val)
+	}
+
+	newNode := &Linkedlist[T]{Item: val}
+	newNode.length = l.length + 1
+	newNode.Head = l.Head
+	l.next = newNode
+	return newNode
+}
+
+func (l *Linkedlist[T]) GetAt(idx int) (T, error) {
+	var link *Linkedlist[T]
+
+	for range idx {
+		link = l.next
+		if link == nil {
+			var zero T
+			return zero, errors.New("index out of range")
+		}
+	}
+
+	return link.Item, nil
+}
--- go/introductoin-to-algorithms/linkedList/linkedList_test.go
+++ go/introductoin-to-algorithms/linkedList/linkedList_test.go
@@ -1,1 +1,30 @@
 package linkedlist_test
+
+import (
+	"slices"
+	"testing"
+
+	linkedlist "ita/linkedList"
+)
+
+func TestBuildAndGetAll(t *testing.T) {
+	starter := linkedlist.Linkedlist[int]{}
+	lL := starter.Build([]int{1, 3, 4, 5, 2, 6, 8, 5, 6, 9, 11})
+
+	all := lL.GetAll()
+
+	randItem := 8
+	if !slices.Contains(all, randItem) {
+		t.Fatalf("did not build the slice correctly, missing %v ", randItem)
+	}
+
+	firstItem := 1
+	if !slices.Contains(all, firstItem) {
+		t.Fatalf("did not build the slice correctly, missing %v ", firstItem)
+	}
+
+	lastItem := 11
+	if !slices.Contains(all, lastItem) {
+		t.Fatalf("did not build the slice correctly, missing %v ", lastItem)
+	}
+}
