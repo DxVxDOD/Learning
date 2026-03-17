cbc852e384ace36de5319f89882b5dcf91463ff3
DxVxDOD <orbangdavid24@gmail.com>
2026-03-17 21:21:13 +0000

    delete first testing started

--- go/introductoin-to-algorithms/doublyLinkedList/doublyLinkedList.go
+++ go/introductoin-to-algorithms/doublyLinkedList/doublyLinkedList.go
@@ -1,7 +1,10 @@
 // Package doublylinkedlist represents a data structure called doubly linked lists. This data structure is sequential but not contiguous in memory.
 package doublylinkedlist
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type node[T any] struct {
 	item T
@@ -22,22 +25,25 @@ func (d *DoublyLinkedList[T]) initWith(val T) {
 	d.lenght = 1
 }
 
-func (d *DoublyLinkedList[T]) InsertLast(val T) {
-	if d.lenght == 0 {
-		d.initWith(val)
+func (d *DoublyLinkedList[T]) walkTo(idx int) *node[T] {
+	var curr *node[T]
+	if d.lenght/2 > idx {
+		curr = d.tail
+		for range idx {
+			curr = curr.prev
+		}
 	} else {
-		oldTail := d.tail
-		d.tail = &node[T]{item: val}
-		oldTail.next = d.tail
-		d.tail.prev = oldTail
-		d.lenght++
+		curr = d.head
+		for range idx {
+			curr = curr.next
+		}
 	}
+
+	return curr
 }
 
-func (d *DoublyLinkedList[T]) Build(arr []T) {
-	for _, v := range arr {
-		d.InsertLast(v)
-	}
+func (d *DoublyLinkedList[T]) Len() int {
+	return d.lenght
 }
 
 func (d *DoublyLinkedList[T]) GetFirst() (T, error) {
@@ -54,6 +60,16 @@ func (d *DoublyLinkedList[T]) GetLast() (T, error) {
 	return d.tail.item, nil
 }
 
+func (d *DoublyLinkedList[T]) GetAt(idx int) (T, error) {
+	if d.lenght <= 0 {
+		return *new(T), errors.New("cannot call GetAt on a empty doubly linked list")
+	}
+	if idx < 0 || idx >= d.lenght {
+		return *new(T), errors.New("index out of bounds")
+	}
+	return d.walkTo(idx).item, nil
+}
+
 func (d *DoublyLinkedList[T]) GetAll() []T {
 	arr := make([]T, d.lenght)
 	curr := d.head
@@ -65,4 +81,90 @@ func (d *DoublyLinkedList[T]) GetAll() []T {
 	}
 
 	return arr
+}
+
+func (d *DoublyLinkedList[T]) InsertLast(val T) {
+	if d.lenght == 0 {
+		d.initWith(val)
+	} else {
+		oldTail := d.tail
+		d.tail = &node[T]{item: val}
+		oldTail.next = d.tail
+		d.tail.prev = oldTail
+		d.lenght++
+	}
+}
+
+func (d *DoublyLinkedList[T]) InsertFirst(val T) {
+	if d.lenght == 0 {
+		d.initWith(val)
+	} else {
+		oldHead := d.head
+		d.head = &node[T]{item: val}
+		d.head.next = oldHead
+		oldHead.prev = d.head
+		d.lenght++
+	}
+}
+
+func (d *DoublyLinkedList[T]) InsertAt(val T, idx int) error {
+	if d.lenght == 0 && idx == 0 {
+		d.initWith(val)
+		return nil
+	}
+	if idx < 0 || idx > d.lenght {
+		return errors.New("index out of bounds")
+	}
+
+	if d.lenght == 0 {
+		return fmt.Errorf("you can only insert at 0 on a empty doubly linked list %v", idx)
+	}
+
+	if idx == 0 {
+		d.InsertFirst(val)
+		return nil
+	}
+	if idx == d.lenght {
+		d.InsertLast(val)
+		return nil
+	}
+
+	nodeAtIdx := d.walkTo(idx)
+	prevNode := nodeAtIdx.prev
+
+	newNode := &node[T]{item: val}
+	newNode.prev = prevNode
+	newNode.next = nodeAtIdx
+
+	nodeAtIdx.prev = newNode
+	prevNode.next = newNode
+
+	d.lenght++
+
+	return nil
+}
+
+func (d *DoublyLinkedList[T]) Build(arr []T) {
+	for _, v := range arr {
+		d.InsertLast(v)
+	}
+}
+
+func (d *DoublyLinkedList[T]) DeleteFirst() (T, error) {
+	if d.lenght == 0 {
+		return *new(T), errors.New("cannot delete from an empty list")
+	}
+
+	if d.lenght == 1 {
+		item := d.head.item
+		d.head = nil
+		d.tail = nil
+		return item, nil
+	}
+
+	nodeToBeDeleted := d.head
+	d.head = nodeToBeDeleted.next
+	d.head.prev = nil
+
+	return nodeToBeDeleted.item, nil
 }
---
+++ go/introductoin-to-algorithms/doublyLinkedList/doublyLinkedListPublic_test.go
@@ -0,0 +1,267 @@
+package doublylinkedlist_test
+
+import (
+	"slices"
+	"testing"
+
+	doublylinkedlist "ita/doublyLinkedList"
+)
+
+func TestGetMethods(t *testing.T) {
+	dl := doublylinkedlist.DoublyLinkedList[int]{}
+	dl.Build([]int{1, 2, 3, 4, 5, 6, 7, 8})
+
+	first, err := dl.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+	last, err := dl.GetLast()
+	if err != nil {
+		t.Fatal(err)
+	}
+	at, err := dl.GetAt(4)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if first != 1 {
+		t.Fatalf("first value %v should be equal to 1", first)
+	}
+
+	if last != 8 {
+		t.Fatalf("last value %v should be equal to 8", last)
+	}
+
+	if at != 5 {
+		t.Fatalf("at value %v should be equal to 8", at)
+	}
+
+	_, err = dl.GetAt(11)
+	if err == nil {
+		t.Fatalf("it should be index out of bounds")
+	}
+
+	_, err = dl.GetAt(-2)
+	if err == nil {
+		t.Fatalf("it should be index out of bounds")
+	}
+}
+
+func TestEmptyGetMethods(t *testing.T) {
+	emptyDL := doublylinkedlist.DoublyLinkedList[int]{}
+
+	_, err := emptyDL.GetFirst()
+	if err == nil {
+		t.Fatal("GetFirst should err on an empty list")
+	}
+
+	_, err = emptyDL.GetLast()
+	if err == nil {
+		t.Fatal("GetLast should err on an empty list")
+	}
+
+	_, err = emptyDL.GetAt(1)
+	if err == nil {
+		t.Fatal("GetAt should err on an empty list")
+	}
+}
+
+func TestBuild(t *testing.T) {
+	dl := doublylinkedlist.DoublyLinkedList[int]{}
+	dl.Build([]int{1, 2, 3, 4, 5, 6, 7, 8})
+
+	all := dl.GetAll()
+	first, err := dl.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+	last, err := dl.GetLast()
+	if err != nil {
+		t.Fatal(err)
+	}
+	at, err := dl.GetAt(4)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !slices.Contains(all, first) {
+		t.Fatalf("all should contain the first element %v", first)
+	}
+
+	if !slices.Contains(all, last) {
+		t.Fatalf("all should contain the last element %v", last)
+	}
+
+	if !slices.Contains(all, at) {
+		t.Fatalf("all should contain the at element %v", at)
+	}
+}
+
+func TestInsertFirst(t *testing.T) {
+	dl := doublylinkedlist.DoublyLinkedList[int]{}
+	dl.Build([]int{1})
+	starterLen := dl.Len()
+	firstVal := 10
+
+	dl.InsertFirst(10)
+	afterLen := dl.Len()
+
+	first, err := dl.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if afterLen <= starterLen {
+		t.Fatalf("starterLen %v should be smaller than after len %v", starterLen, afterLen)
+	}
+
+	if afterLen != starterLen+1 {
+		t.Fatalf("afterLen %v should be starterLen %v + 1", afterLen, starterLen)
+	}
+
+	if first != firstVal {
+		t.Fatalf("get first value %v should equal the value inserted %v", first, firstVal)
+	}
+
+	emptyDL := doublylinkedlist.DoublyLinkedList[int]{}
+	secondVal := 20
+	emptyDL.InsertFirst(secondVal)
+
+	secondFirst, err := emptyDL.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if secondFirst != secondVal {
+		t.Fatalf("second val %v should equal second get first %v", secondVal, secondFirst)
+	}
+}
+
+func TestInsertLast(t *testing.T) {
+	dl := doublylinkedlist.DoublyLinkedList[int]{}
+	dl.Build([]int{1})
+	starterLen := dl.Len()
+	lastVal := 10
+
+	dl.InsertLast(10)
+	afterLen := dl.Len()
+
+	last, err := dl.GetLast()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if afterLen <= starterLen {
+		t.Fatalf("starterLen %v should be smaller than after len %v", starterLen, afterLen)
+	}
+
+	if afterLen != starterLen+1 {
+		t.Fatalf("afterLen %v should be starterLen %v + 1", afterLen, starterLen)
+	}
+
+	if last != lastVal {
+		t.Fatalf("get last value %v should equal the value inserted %v", last, lastVal)
+	}
+
+	emptyDL := doublylinkedlist.DoublyLinkedList[int]{}
+	secondVal := 20
+	emptyDL.InsertLast(secondVal)
+
+	secondLast, err := emptyDL.GetLast()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if secondLast != secondVal {
+		t.Fatalf("second val %v should equal second get last %v", secondVal, secondLast)
+	}
+}
+
+func TestInsertAt(t *testing.T) {
+	dl := &doublylinkedlist.DoublyLinkedList[int]{}
+	dl.Build([]int{1, 2, 3, 4, 5, 6, 7, 8})
+	starteLen := dl.Len()
+	atVal := 10
+	atIdx := 4
+
+	err := dl.InsertAt(atVal, atIdx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	afterLen := dl.Len()
+
+	if afterLen <= starteLen {
+		t.Fatalf("starteLen %v should be smaller than afterLen %v", starteLen, afterLen)
+	}
+
+	if afterLen != starteLen+1 {
+		t.Fatalf("starteLen %v should be only one smaller that afterLen %v", starteLen, afterLen)
+	}
+
+	getAtVal, err := dl.GetAt(atIdx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if getAtVal != atVal {
+		t.Fatalf("getAtVal %v should equal the value inserted %v", getAtVal, atVal)
+	}
+
+	firstAtVal := 11
+	err = dl.InsertAt(firstAtVal, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	first, err := dl.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if first != firstAtVal {
+		t.Fatalf("get first value %v should equal the inserted value %v", first, firstAtVal)
+	}
+
+	lastAtVal := 12
+	err = dl.InsertAt(lastAtVal, dl.Len())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	last, err := dl.GetLast()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if last != lastAtVal {
+		t.Fatalf("get last value %v should equal the inserted value %v", last, lastAtVal)
+	}
+}
+
+func TestInserAtEmpty(t *testing.T) {
+	emptyDL := doublylinkedlist.DoublyLinkedList[int]{}
+
+	emptyVal := 10
+	emptyAtIdx := 11
+	err := emptyDL.InsertAt(emptyVal, emptyAtIdx)
+	if err == nil {
+		t.Fatalf("You should not be able to insert above 0 on ane empty list, idx %v", emptyAtIdx)
+	}
+
+	err = emptyDL.InsertAt(emptyVal, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	firstEmpty, err := emptyDL.GetFirst()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if firstEmpty != emptyVal {
+		t.Fatalf("the first value %v in the list should equal the inserted value %v", firstEmpty, emptyVal)
+	}
+}
+
+func TestDeleteFirst(t *testing.T) {
+}
