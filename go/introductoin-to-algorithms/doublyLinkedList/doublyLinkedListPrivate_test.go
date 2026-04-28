67af3adb9f7c51e97e8c339e022cf4372720d85e
David Orban <orbangdavid24@gmail.com>
2026-04-28 20:59:36 +0100

    pulling main

--- go/introductoin-to-algorithms/doublyLinkedList/doublyLinkedListPrivate_test.go
+++ go/introductoin-to-algorithms/doublyLinkedList/doublyLinkedListPrivate_test.go
@@ -9,4 +9,8 @@ func TestWalkTo(t *testing.T) {
 	dl.Build([]int{1, 2, 3, 4, 5, 6, 7, 8})
 
 	dlNode := dl.walkTo(4)
+
+	if dlNode.item != 5 {
+		t.Fatalf("item %v should equal the 4th element", dlNode.item)
+	}
 }
