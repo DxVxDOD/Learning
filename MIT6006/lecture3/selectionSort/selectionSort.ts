858e0a37d8ea76879d8c91c71901e8d62e1dbf10
David Orban <orbangdavid24@gmail.com>
2026-06-21 15:07:02 +0100

    pushing sort

--- MIT6006/lecture3/selectionSort/selectionSort.ts
+++ MIT6006/lecture3/selectionSort/selectionSort.ts
@@ -1,23 +1,3 @@
-function getCurrMax(i: number, arr: number[]): number {
-    if (i > 0) {
-        const j = getCurrMax(i - 1, arr)
-        if (arr[j] > arr[i]) return j
-    }
-    return i
-}
+export function selectionSort(arr: number[], idx = -1) {
 
-export function selectionSort(arr: number[], i = -1) {
-    if (i < 0) {
-        i = arr.length - 1
-    }
-    if (i > 0) {
-        const j = getCurrMax(i, arr)
-        if (i !== j) {
-            arr[i] = arr[j] + arr[i]
-            arr[j] = arr[i] - arr[j]
-            arr[i] = arr[i] - arr[j]
-        }
-        selectionSort(arr, i - 1)
-    }
-    return arr
 }
